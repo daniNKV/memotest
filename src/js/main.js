0eaf1ec32c8a834d8555e64ee40cbd213937b236
daniNKV <nikiforovdaniel@gmail.com>
2022-04-23 20:06:36 -0300

    Creando la lista con la ubicación aleatoria de los colores

--- src/js/main.js
+++ src/js/main.js
@@ -6,17 +6,58 @@ const DOM = {
 
 }
 const config = {
-    colors : ['rojo', 'amarillo', 'verde', 'azul', 'magenta', 'violenta'],
+    colors : {
+        'red' : '',
+        'yellow' : '' ,
+        'green' : '',
+        'blue' : '' ,
+        'orange' : '', 
+        'pink' : ''
+    },
 
-    cardsAmount : 12
-
+    cardsAmount : 12,
+ 
 }
+
+function initRound(config) {
+    const colorsLocation = shuffle(createColors(config))
+}
+
+initRound(config)
+
+
+
+function shuffle(arrayToShuffle, n) {
+    let array = arrayToShuffle
+
+    for (let i = n - 1; i > 0; i--){
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return arrayToShuffle
+    
+}
+
+
+function createColors(config) {
+    const {colors, cardsAmount} = config
+
+    return createColorList(colors, cardsAmount)
+}
+
+
+function createColorList(colorsObj, amount) { 
+    const colors =  Object.keys(colorsObj)
+    return colors.slice(0, amount).concat(colors)
+}
+
+
 
 
 document.addEventListener('DOMContentLoaded', () => placeCards(config.cardsAmount))
 
 function placeCards(acc, parent = DOM.table) {
-    for(let i = acc ; i > 0 ; i--) {
+    for(let i = 0 ; i <= acc ; i++) {
         parent.appendChild(createCard(DOM.cardTemplate, i))
     }
 
