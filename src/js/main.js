a24a1e139238d1ae401f265226b78fe0b1c6de6d
daniNKV <nikiforovdaniel@gmail.com>
2022-04-23 02:05:48 -0300

    Cartas creadas dinámicamente

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,34 @@
-let $cards = document.querySelectorAll('.card')
 
-$cards[0].addEventListener('click', e => e.target.parentNode.classList.toggle('show'))
-console.log($cards)
+const DOM = {
+    cardTemplate : document.getElementById('card'),
+    table : document.getElementById('table')
+
+
+}
+const config = {
+    colors : ['rojo', 'amarillo', 'verde', 'azul', 'magenta', 'violenta'],
+
+    cardsAmount : 12
+
+}
+
+
+document.addEventListener('DOMContentLoaded', () => placeCards(config.cardsAmount))
+
+function placeCards(acc, parent = DOM.table) {
+    for(let i = acc ; i > 0 ; i--) {
+        parent.appendChild(createCard(DOM.cardTemplate, i))
+    }
+
+}
+
+function createCard(template, id) {
+    let $card = template.content.firstElementChild.cloneNode(true)
+
+    $card.id = `${ id }`
+
+    return $card
+}
+
+
 
