b937ac894d0b76e47870a4b1ceab669fdf9395c5
daniNKV <nikiforovdaniel@gmail.com>
2022-05-10 23:38:33 -0300

    Paint and compare two clicks

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,93 @@
 
 const DOM = {
     cardTemplate : document.getElementById('card'),
-    table : document.getElementById('table')
+    table : document.getElementById('table'),
+}   
 
 
-}
 const config = {
     colors : {
-        'red' : '',
-        'yellow' : '' ,
-        'green' : '',
-        'blue' : '' ,
-        'orange' : '', 
-        'pink' : ''
+        'red' : 'bg-red-600',
+        'yellow' : 'bg-yellow-600' ,
+        'green' : 'bg-green-600',
+        'blue' : 'bg-blue-600' ,
+        'orange' : 'bg-orange-600', 
+        'purple' : 'bg-purple-600'
     },
 
     cardsAmount : 12,
  
 }
 
-function initRound(config) {
-    const colorsLocation = shuffle(createColors(config))
+document.addEventListener('DOMContentLoaded', () => initGame())
+ 
+
+function initGame() {
+    placeCards(config.cardsAmount)
+
+    initRound(shuffle(createColors(config)), n = config.cardsAmount)
+    
+
+    // elements.forEach(card => card.addEventListener('click', e => verifyPair(e, solution, elements)))
+    // DOM.table.addEventListener('click', e => e.target != DOM.table ? console.log(e.target.parentNode.id) : '')
+}
+function getInput(solution) {
+    return e => {
+        paintCard(solution[e.target.id], e.target)
+        captureInput(e.target.id, solution)} 
 }
 
-initRound(config)
+function paintCard(color, card){
+    card.nextElementSibling.classList.toggle(config.colors[color])
+}
+
+function captureInput(firstClick, solution){
+    const getNextInput = (solution) => (e) => {
+        paintCard(solution[e.target.id], e.target)
+        compareClicks(firstClick, e.target.id, solution)
+    }
+    
+    enableInput(getNextInput, solution)
+
+    console.log('f', firstClick)
+}
+
+function compareClicks(first, second, solution) {
+    if (solution[first] === solution[second]) {
+        console.log('true')
+    }
+    else 
+        console.log('false')
+}
+
+function initRound(solution, colorsLeft){
+    if (colorsLeft > 0) {
+        enableInput(getInput, solution)
+    }
+    // const compareClicks = (secondClick) => {
+
+    // }
+
+}
 
 
+// const var1 = comparar(2)
+// const var2 = var1(4)
+
+// console.log(var2)
+
+function enableInput(fn, data) {
+    // elements.forEach(card => card.addEventListener('click', e => fn(e)))
+   // DOM.table.addEventListener('click', e => e.target !== DOM.table ? fn(e) : '')
+   DOM.table.onclick = fn(data)
+}
+
+function disableInput(fn) {
+    // elements.forEach(card => card.removeEventListener('click', fn))
+   // DOM.table.removeEventListener('click', fn)
+    DOM.table.onclick = fn
+
+}
 
 function shuffle(arrayToShuffle, n) {
     let array = arrayToShuffle
@@ -34,8 +96,7 @@ function shuffle(arrayToShuffle, n) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-    return arrayToShuffle
-    
+    return array 
 }
 
 
@@ -47,29 +108,26 @@ function createColors(config) {
 
 
 function createColorList(colorsObj, amount) { 
-    const colors =  Object.keys(colorsObj)
-    return colors.slice(0, amount).concat(colors)
+    return Object.keys(colorsObj).slice(0, amount).concat(Object.keys(colorsObj))
 }
 
-
-
-
-document.addEventListener('DOMContentLoaded', () => placeCards(config.cardsAmount))
-
-function placeCards(acc, parent = DOM.table) {
-    for(let i = 0 ; i <= acc ; i++) {
+function placeCards(amount, parent = DOM.table) {
+    for(let i = 0 ; i < amount ; i++) {
         parent.appendChild(createCard(DOM.cardTemplate, i))
     }
 
+    return Array.from(document.querySelectorAll('.card-inner'))
 }
 
 function createCard(template, id) {
     let $card = template.content.firstElementChild.cloneNode(true)
 
-    $card.id = `${ id }`
+    $card.children[0].children[0].id = `${ id }`
+    $card.onclick = (e) => e.target.parentNode.classList.toggle('show')
 
     return $card
 }
+
 
 
 
