b4e153a5e44d72c9668d62784af7d2e2fcb5ee87
daniNKV <nikiforovdaniel@gmail.com>
2022-10-19 12:37:29 -0300

    E2E test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@ describe('Gameplay Test', () => {
 	
 	
 	it('loads', () => {
-		cy.visit('http://192.168.0.228:8080/')
+		cy.visit('http://127.0.0.1:5500/index.html')
 	})
 	
 	describe('UI', () => {
@@ -16,7 +16,8 @@ describe('Gameplay Test', () => {
 	
 	
 	describe('Walktrough', () => {
-		const solution = ["red", "pink", "red", "purple", "orange", "pink", "orange", "blue", "green", "purple", "green", "blue"]
+		//const solution = [[0,2], [1,5], [3,9], [4,6], [7,11], [8,10]]
+		const cards = [[1,5], [3,9], [4,6], [7,11], [8,10]]
 
 		describe('Wrong pair', () => {
 			it('Click first card', () => {
@@ -44,11 +45,54 @@ describe('Gameplay Test', () => {
 				cy.get('.card-back').should('have.class', 'bg-blue-600')
 			})
 			it('Table change', () => {
-				cy.get('#table').find('.card-front').filter('.founded').should('have.length', 2	)
-				cy.get('#table').find('card-back').should('have.length', 2)
+				cy.get('#table').find('.card-inner').filter('.win').should('have.length', 2)
+			})
+			it('Move counter change', () => {
+				cy.get('#counter').should('contain', 'Moves: 2')
 			})
 			})
+
+		describe('Finish game', () => {
+			it('Second pair', () => {
+				cy.get('#table').children('.card').find('.card-front').eq(cards[0][0]).click()
+				cy.get('#table').children('.card').find('.card-front').eq(cards[0][1]).click()
+
+			})
+			it('Third pair', () => {
+				cy.get('#table').children('.card').find('.card-front').eq(cards[1][0]).click()
+				cy.get('#table').children('.card').find('.card-front').eq(cards[1][1]).click()
+
+			})
+			it('Fourth pair', () => {
+				cy.get('#table').children('.card').find('.card-front').eq(cards[2][0]).click()
+				cy.get('#table').children('.card').find('.card-front').eq(cards[2][1]).click()
+
+			})
+			it('Fifth pair', () => {
+				cy.get('#table').children('.card').find('.card-front').eq(cards[3][0]).click()
+				cy.get('#table').children('.card').find('.card-front').eq(cards[3][1]).click()
+
+			})
+			it('Sixth pair', () => {
+				cy.get('#table').children('.card').find('.card-front').eq(cards[4][0]).click()
+				cy.get('#table').children('.card').find('.card-front').eq(cards[4][1]).click()
+
+			})
+		})
+		describe('Modal at finished game', () => {
+			it ('Exists', () => {
+				cy.get('#modal')
+			})
+			it ('Correct moves count', () => {
+				cy.get('#moves-made').should('contain', 7)
+			})
+			it('Exists button for play again', () => {
+				cy.get('#restart').should('contain', 'Play Again')
+			})
+		})
 	})
 
 	
 })
+
+//    solution: ["red", "pink", "red", "purple", "orange", "pink", "orange", "blue", "green", "purple", "green", "blue"],
